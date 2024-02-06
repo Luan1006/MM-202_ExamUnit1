ac3b2c2cecd2f52ef1eb773b69d9348131b4ab85
Luan1006 <thenoobsisbest@gmail.com>
2024-02-06 14:22:07 +0100

    Surrounded Task5 in a class

--- Task5.cs
+++ Task5.cs
@@ -1,74 +1,81 @@
-while (!AtGoal())
+class Task5
 {
-    if (Peek())
+    static void Main()
     {
-        Move();
-        Paint();
-    }
-    else
-    {
-        Turn();
-    }
-
-    PeekAllDirections();
-}
-
-#region Basic functions
-// These functions are just her to make your intelisense work. 
-// They only have a conceptual function.
-
-void Move()
-{
-    // Moves the car 1 cell in the direction it is heading. 
-}
-
-void Turn()
-{
-    // Turns the car 90 deg counter clockwise.
-}
-
-bool Peek()
-{
-    // Returns true if the next cell is open, otherwise false.
-    return true; // Just a placeholder value. 
-}
-
-#endregion
-
-#region Created functions
-
-void PeekAllDirections()
-{
-    int turnCount = 0;
-
-    while (turnCount < 4)
-    {
-        if (IsGoal())
+        // Your code here
+        while (!AtGoal())
         {
-            Move();
+            if (Peek())
+            {
+                Move();
+                Paint();
+            }
+            else
+            {
+                Turn();
+            }
+
+            PeekAllDirections();
         }
 
-        Turn();
+        #region Basic functions
+        // These functions are just her to make your intelisense work. 
+        // They only have a conceptual function.
 
-        turnCount++;
+        void Move()
+        {
+            // Moves the car 1 cell in the direction it is heading. 
+        }
+
+        void Turn()
+        {
+            // Turns the car 90 deg counter clockwise.
+        }
+
+        bool Peek()
+        {
+            // Returns true if the next cell is open, otherwise false.
+            return true; // Just a placeholder value. 
+        }
+
+        #endregion
+
+        #region Created functions
+
+        void PeekAllDirections()
+        {
+            int turnCount = 0;
+
+            while (turnCount < 4)
+            {
+                if (IsGoal())
+                {
+                    Move();
+                }
+
+                Turn();
+
+                turnCount++;
+            }
+        }
+
+        bool IsGoal()
+        {
+            // Returns true if the cell ahead is the goal cell.
+            return true; // Just a placeholder value.
+        }
+
+        bool AtGoal()
+        {
+            // Returns true if the current cell is the goal cell.
+            return true; // just a placholder
+        }
+
+        void Paint()
+        {
+            // Paints the current cell. Making it a wall.
+        }
+
+        #endregion
     }
 }
-
-bool IsGoal()
-{
-    // Returns true if the cell ahead is the goal cell.
-    return true; // Just a placeholder value.
-}
-
-bool AtGoal()
-{
-    // Returns true if the current cell is the goal cell.
-    return true; // just a placholder
-}
-
-void Paint()
-{
-    // Paints the current cell. Making it a wall.
-}
-
-#endregion
