9dbd86bcab7cec636b95745258c41c0946b2fcf2
Luan1006 <thenoobsisbest@gmail.com>
2024-02-06 14:27:52 +0100

    Surrounded Task5 in a class

--- Task5.cs
+++ Task5.cs
@@ -1,61 +1,67 @@
-while (!AtGoal())
+class Task5
 {
-    TurnCounterClockwise();
-    if (Peek())
+    void Main()
     {
-        Move();
-    }
-    else
-    {
-        Turn();
-        if(Peek())
+        while (!AtGoal())
         {
-            Move();
+            TurnCounterClockwise();
+            if (Peek())
+            {
+                Move();
+            }
+            else
+            {
+                Turn();
+                if (Peek())
+                {
+                    Move();
+                }
+                else
+                {
+                    Turn();
+                    Move();
+                }
+            }
         }
-        else
+
+        #region Basic functions
+        // These functions are just her to make your intelisense work. 
+        // They only have a conceptual function.
+
+        void Move()
         {
-            Turn();
-            Move();
+            // Moves the car 1 cell in the direction it is heading. 
         }
+
+        void Turn()
+        {
+            // Turns the car 90 deg counter clockwise.
+        }
+
+        bool Peek()
+        {
+            // Returns true if the next cell is open, otherwise false.
+            return true; // Just a placeholder value. 
+        }
+
+        bool AtGoal()
+        {
+            // Returns true if the current cell is the goal cell.
+            return true; // just a placholder
+        }
+
+        #endregion
+
+        #region Created Functions
+
+        void TurnCounterClockwise()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                Turn();
+            }
+        }
+
+        #endregion
     }
 }
-
-#region Basic functions
-// These functions are just her to make your intelisense work. 
-// They only have a conceptual function.
-
-void Move()
-{
-    // Moves the car 1 cell in the direction it is heading. 
-}
-
-void Turn()
-{
-    // Turns the car 90 deg counter clockwise.
-}
-
-bool Peek()
-{
-    // Returns true if the next cell is open, otherwise false.
-    return true; // Just a placeholder value. 
-}
-
-bool AtGoal()
-{
-    // Returns true if the current cell is the goal cell.
-    return true; // just a placholder
-}
-
-#endregion
-
-#region Created Functions
-
-void TurnCounterClockwise()
-{
-    for (int i = 0; i < 4; i++)
-    {
-        Turn();
-    }
-}
-
-#endregion
